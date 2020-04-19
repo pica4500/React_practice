ac668c479388974a8d673c037b91b25706a6f2bd
pica4500 <pica4500@gmail.com>
2020-04-19 18:34:43 +0900

    append character array and print all characters

--- src/App.js
+++ src/App.js
@@ -2,32 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
+class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: 'Hello pica4500'
+      characters: [
+        {
+          name: '시오리'
+        },
+        {
+          name: '사렌'
+        },
+        {
+          name: '쿄우카'
+        },
+      ]
     }
   }
-  render(){
+  render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.state.string}
-        </p>
-        <button onClick={() => this.setState({ string: 'Hello pica4501'})}>Change Text</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        {
+          this.state.characters.map(character => <h1> {character.name} </h1>)
+        }
+      </div>
     );
   }
 }
